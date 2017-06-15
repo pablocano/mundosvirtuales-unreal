02607b3171a388dece49ddc291d8da5e6fa260eb
Pablo Cano <Pablo.cano@mundos-virtuales.com>
2017-06-15 12:08:19 -0400

    Client submodule added

--- ThirdParty/PlantCore/Src/Utils/Security/Cryptography.cpp
+++ ThirdParty/PlantCore/Src/Utils/Security/Cryptography.cpp
@@ -1,7 +1,8 @@
 #include "Cryptography.h"
-  #include <stdint.h>
-  #define DELTA 0x9e3779b9
-  #define MX (((z>>5^y<<2) + (y>>3^z<<4)) ^ ((sum^y) + (key[(p&3)^e] ^ z)))
+#include <stdint.h>
+
+#define DELTA 0x9e3779b9
+#define MX (((z>>5^y<<2) + (y>>3^z<<4)) ^ ((sum^y) + (key[(p&3)^e] ^ z)))
 
 void Cryptography::encrypt(uint32_t *v, int n, uint32_t const key[4])
 {
